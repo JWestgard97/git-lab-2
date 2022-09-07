4245597ef5bf35d5dfa79603ed19cd3f43bcdf97
Jim <jw878121@gmail.com>
2022-09-07 17:46:30 -0400

    adding my c++, hrllo git, program

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *
+ *       File: git-lab-program.cc
+ *     Author: Jim Westgard
+ *       Date: 07/09/2022
+ *Description: Add description
+*/
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+int main (int argc, char const *argv[]){
+    cout << "Hello Git!!" <<endl;
+    return 0;
+
+}// main
+
